b1aef65193528e8e3920fecb2ff7dcb87ecd0e02
kuroi-okami <>
2020-04-23 21:05:36 +0100

    Reintroduce slow tests for insertion sort

--- test/sorting/test_insertion_sort.py
+++ test/sorting/test_insertion_sort.py
@@ -33,10 +33,9 @@ class TestQuickSort:
 
         assert testList == expectedResults
 
-    """
     @pytest.mark.slow
     def test_insertion_sort_very_large_list(self):
-        testRange = range(0, 1000000)
+        testRange = range(0, 10000)
         testList = list(testRange)
         shuffle(testList)  # Shuffle the deck
         expectedResults = list(testRange)
@@ -48,13 +47,12 @@ class TestQuickSort:
 
     @pytest.mark.slow
     def test_insertion_sort_large_killer_list(self):
-        testRange = range(0, 1000000)
+        testRange = range(0, 10000)
         testList, expectedResults = list(reversed(testRange)), list(testRange)
         # Invoke method under test
         InsertionSort(testList)
 
         assert testList == expectedResults
-    """
 
     def test_insertion_sort_already_sorted_small(self):
         testRange = range(0, 1000)
@@ -66,7 +64,7 @@ class TestQuickSort:
 
     @pytest.mark.slow
     def test_insertion_sort_already_sorted_large(self):
-        testRange = range(0, 1000000)
+        testRange = range(0, 10000)
         testList, expectedResults = list(testRange), list(testRange)
         # Invoke method under test
         InsertionSort(testList)
